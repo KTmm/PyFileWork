1932bf428eb1614af594e9e5d035dda5b3a3f993
Ying Zhou <cherubzhou@gmail.com>
2015-07-21 18:33:36 -0400

    init

---
+++ findSpecificFunctionInJBITe.py
@@ -0,0 +1,13 @@
+import os
+import sys
+import csv
+
+path = "/Users/yingzhou/Documents/projects/my.cs/tests/jbite"
+
+for i in range:
+    for folders, subs, files in os.walk(rootdir):
+        if files != []:
+            for file in files:
+                if "xlsx" in file:
+                    
+                
